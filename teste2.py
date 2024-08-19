49892c8f54a4e1f9948b1be5dfcb01f5e704d853
Edasuno <Edasuno@users.noreply.github.com>
2024-08-19 00:00:11 -0300

    teste profundo

---
+++ teste2.py
@@ -0,0 +1,9 @@
+# Solicita dois números
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Soma os números
+soma = num1 + num2
+
+# Exibe o resultado
+print("A soma é:", soma)
